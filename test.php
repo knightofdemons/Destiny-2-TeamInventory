e794c1e4f1e0a2bc0e660088b96126a0d48f09d4
knightofdemons <8983377+knightofdemons@users.noreply.github.com>
2021-10-21 04:23:43 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,24 @@
+<?php
+$apiKey = $_GET["apikey"];
+$url = 'https://www.bungie.net/Platform/User/Search/Prefix/Hühnchen/0/';
+
+$ch = curl_init($url);
+
+$options = array(   CURLOPT_HTTPGET => true,
+                    CURLOPT_HTTPHEADER => array('X-API-Key: ' . $apiKey),
+                    CURLOPT_RETURNTRANSFER => true
+                );
+                
+curl_setopt_array(
+                    $ch,
+                    $options
+                );
+
+$response_json = curl_exec($ch);
+
+curl_close($ch);
+
+$response = json_decode($response_json, true);
+
+echo '<pre>'; print_r($response); echo '</pre>';
+?>
