9b1a350a42274b0f46b23324106041c45de3573e
knightofdemons <8983377+knightofdemons@users.noreply.github.com>
2021-10-21 08:03:56 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<header>
+	<div class="search">
+		<button id="closeButton" class="close-button" type="button">
+			<img src="https://assets.codepen.io/907368/close_1.svg">
+		</button>
+		<div class="searchbar">
+			<form id="searchform">
+				<input
+						 placeholder="Dein Suchbegriff"
+						 type="search"
+						 id="mainSearch"
+						 name="mainSearch"
+						 class="close">
+				<button type="submit" id="searchButton">
+					<img src="https://assets.codepen.io/907368/search.svg">
+				</button>
+			</form>
+		</div>
+	</div>
+</header>
