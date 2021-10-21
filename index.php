8d3723d5545de2fd479b0d9881d18ccfbbc53fae
knightofdemons <8983377+knightofdemons@users.noreply.github.com>
2021-10-21 09:09:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <header>
 	<script type="text/javascript" src="search.js"></script>
-	<link rel="stylesheet/less" type="text/css" href="search.css" /> <script src="https://cdn.jsdelivr.net/npm/less@4.1.1" ></script>
+	<link rel="stylesheet/less" type="text/css" href="search.css" /></script>
 	<div class="search">
 		<button id="closeButton" class="close-button" type="button">
-			<img src="https://assets.codepen.io/907368/close_1.svg">
+			<img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMjQgMjAuMTg4bC04LjMxNS04LjIwOSA4LjItOC4yODItMy42OTctMy42OTctOC4yMTIgOC4zMTgtOC4zMS04LjIwMy0zLjY2NiAzLjY2NiA4LjMyMSA4LjI0LTguMjA2IDguMzEzIDMuNjY2IDMuNjY2IDguMjM3LTguMzE4IDguMjg1IDguMjAzeiIvPjwvc3ZnPg==">
 		</button>
 		<div class="searchbar">
 			<form id="searchform">
@@ -14,7 +14,7 @@
 						 name="mainSearch"
 						 class="close">
 				<button type="submit" id="searchButton">
-					<img src="https://assets.codepen.io/907368/search.svg">
+					<img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMjMuODIyIDIwLjg4bC02LjM1My02LjM1NGMuOTMtMS40NjUgMS40NjctMy4yIDEuNDY3LTUuMDU5LjAwMS01LjIxOS00LjI0Ny05LjQ2Ny05LjQ2OC05LjQ2N3MtOS40NjggNC4yNDgtOS40NjggOS40NjhjMCA1LjIyMSA0LjI0NyA5LjQ2OSA5LjQ2OCA5LjQ2OSAxLjc2OCAwIDMuNDIxLS40ODcgNC44MzktMS4zMzNsNi4zOTYgNi4zOTYgMy4xMTktMy4xMnptLTIwLjI5NC0xMS40MTJjMC0zLjI3MyAyLjY2NS01LjkzOCA1LjkzOS01LjkzOCAzLjI3NSAwIDUuOTQgMi42NjQgNS45NCA1LjkzOCAwIDMuMjc1LTIuNjY1IDUuOTM5LTUuOTQgNS45MzktMy4yNzQgMC01LjkzOS0yLjY2NC01LjkzOS01LjkzOXoiLz48L3N2Zz4=">
 				</button>
 			</form>
 		</div>
