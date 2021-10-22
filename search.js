3ff4e27ba34db225752cf9d1ab95b6cdea9f7ef2
knightofdemons <8983377+knightofdemons@users.noreply.github.com>
2021-10-22 04:19:31 +0200

    Update search.js

--- search.js
+++ search.js
@@ -1,65 +1,64 @@
+"use strict";
 
 const SEARCH = document.querySelector('.search');
 const MAINSEARCH = document.querySelector('#mainSearch');
 const SEARCHBUTTON = document.querySelector('#searchButton');
 const CLOSEBUTTON = document.querySelector('#closeButton');
 const SEARCHFORM = document.querySelector('#searchform');
+SEARCHFORM.addEventListener('submit', $event => {
+  $event.preventDefault();
+  var data = new FormData(SEARCHFORM);
 
-SEARCHFORM.addEventListener('submit', ($event) => {
+  for (const [name, value] of data) {
+    search(value);
+  }
+});
+SEARCHBUTTON.addEventListener('click', $event => {
+  if (!searchIsOpen()) {
     $event.preventDefault();
-    var data = new FormData(SEARCHFORM);
-    for (const [name, value] of data) {
-        search(value);
-    }
+    openSearchbar();
+  }
 });
-
-SEARCHBUTTON.addEventListener('click', ($event) => {
-    if (!searchIsOpen()) {
-        $event.preventDefault();
-        openSearchbar();
-    }
-});
-
 CLOSEBUTTON.addEventListener('click', () => {
-    closeSearchbar();
+  closeSearchbar();
 });
-
 MAINSEARCH.addEventListener('keyup', () => {
-    search(MAINSEARCH.value)
+  search(MAINSEARCH.value);
 });
 
-window.onkeyup = ($keyboardEvent) => {
-    if (searchShortcut($keyboardEvent)) {
-        openSearchbar();
-    }
-    if (isleavingSearchbar($keyboardEvent)) {
-        closeSearchbar();
-    }
-}; 
+window.onkeyup = $keyboardEvent => {
+  if (searchShortcut($keyboardEvent)) {
+    openSearchbar();
+  }
+
+  if (isleavingSearchbar($keyboardEvent)) {
+    closeSearchbar();
+  }
+};
 
 const searchIsOpen = () => {
-    return SEARCH.classList.contains('open');
-}
+  return SEARCH.classList.contains('open');
+};
 
-const search = (searchString) => {
-    console.log(`Searchstring: ${searchString}`);
-}
+const search = searchString => {
+  console.log(`Searchstring: ${searchString}`);
+};
 
-const searchShortcut = (keyboardEvent) => {
-    return keyboardEvent.code === 'KeyS' && keyboardEvent.target === document.body;
-}
+const searchShortcut = keyboardEvent => {
+  return keyboardEvent.code === 'KeyS' && keyboardEvent.target === document.body;
+};
 
-const isleavingSearchbar = (keyboardEvent) => {
-    return keyboardEvent.code === 'Escape';
-}
+const isleavingSearchbar = keyboardEvent => {
+  return keyboardEvent.code === 'Escape';
+};
 
 const openSearchbar = () => {
-    SEARCH.classList.add('open');
-    MAINSEARCH.focus();
-}
+  SEARCH.classList.add('open');
+  MAINSEARCH.focus();
+};
 
 const closeSearchbar = () => {
-    SEARCH.classList.remove('open');
-    MAINSEARCH.value = '';
-    document.activeElement.blur();
-}
+  SEARCH.classList.remove('open');
+  MAINSEARCH.value = '';
+  document.activeElement.blur();
+};
