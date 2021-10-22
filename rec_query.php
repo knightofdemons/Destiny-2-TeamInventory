2267b9b22c6a705c56784f391961d7a91f7c8480
knightofdemons <8983377+knightofdemons@users.noreply.github.com>
2021-10-22 12:35:21 +0200

    Update rec_query.php

--- rec_query.php
+++ rec_query.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
-
-<head>
-    <title>Stats</title>
-
-    <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <meta name="keywords" content="" />
-
-</head>
-
-<body>
 <?php
 // get API key
 $apiKey = $_GET["apikey"];
@@ -173,5 +159,3 @@ echo "<table style='width:30%'>";
 echo "</table>";
 echo "<br>";
 ?>
-</body>
-</html>
