f09afa65d930522d8be32af17bcd989c7a7741c3
knightofdemons <8983377+knightofdemons@users.noreply.github.com>
2021-10-22 12:02:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,43 +8,52 @@
  </head>
  <body>
 <div class="profile">
-           <div class="name_job">
-             <div class="name">Hühnchen Süß-Sauer 7,99€</div>
-             <div class="job">Warlock</div>
-           </div><img src="https://www.bungie.net/img/profile/avatars/cc000016.jpg" alt="profileImg">
-         <i class='bx bx-log-out' id="log_out" ></i>
+  <div class="name_job">
+    <div class="name">Hühnchen Süß-Sauer 7,99€</div>
+    <div class="plattform"><img id="profilePlat" src="https://cdn.freebiesupply.com/images/large/2x/steam-logo-transparent.png" alt="profilePlat">Steam</div>
+  </div><img id="profilePic" src="https://www.bungie.net/img/profile/avatars/cc000016.jpg" alt="profilePic">
+  <i class='bx bx-log-out' id="logout"></i>
 </div>
 <div class="sidebar">
-    <div class="logo-details">
-      <img src="https://raw.githubusercontent.com/knightofdemons/Destiny-2-TeamInventory/main/favicon.ico"></img>
-        <div class="logo_name">D2 TeamInv</div>
-        <i class='bx bx-menu' id="btn" ></i>
-    </div>
-    <ul class="nav-list">
-      <li>
-          <i class='bx bx-search' ></i>
-         <input type="text" placeholder="Search...">
-         <span class="tooltip">Search</span>
- 	 <script type="text/javascript" src="search.js"></script>
-      </li>
+  <div class="sidebar-logo-details">
+    <img src="https://raw.githubusercontent.com/knightofdemons/Destiny-2-TeamInventory/main/favicon.ico"></img>
+    <div class="sidebar-logo_name">D2 TeamInv</div>
+    <i class='bx bx-menu' id="sidebar-btn"></i>
+  </div>
+  <ul class="nav-list">
+    <li>
+      <i class='bx bx-search'></i>
+      <input type="text" placeholder="Search...">
+      <span class="tooltip">Search</span>
+    </li>
+    <li>
+      <a href="#">
+        <i class='bx bx-heart'></i>
+        <span class="links_name">PLACEHOLDER</span>
+      </a>
+      <span class="tooltip">PLACEHOLDER</span>
+    </li>
+  </ul>
+  <div class="sidebar-settings">
+    <ul class="nav-settings">
       <li>
         <a href="#">
-          <i class='bx bx-heart'></i>
-          <span class="links_name">PLACEHOLDER</span>
+          <i class='bx bx-cog'></i>
+          <span class="links_name">Settings</span>
         </a>
-         <span class="tooltip">PLACEHOLDER</span>
+        <span class="tooltip">Settings</span>
       </li>
-     <li>
-       <a href="#">
-         <i class='bx bx-cog' ></i>
-         <span class="links_name">Settings</span>
-       </a>
-       <span class="tooltip">Settings</span>
-     </li>
     </ul>
   </div>
-  <section class="home-section">
-      <div class="text">Recordprogess here</div>
-  </section>
+</div>
+<section class="main-content">
+  <div class="main-header">
+    <a class="list-header-playername">Accountname</a>
+    </br>
+    <a class="list-header-char1">Char1</a>
+    <a class="list-header-char2">Char2</a>
+    <a class="list-header-char3">Char3</a>
+  </div>
+</section>
  </body>
 </html>
