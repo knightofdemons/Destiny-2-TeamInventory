e82fde976f3d2f4942d0a9e249ec310890635b4d
knightofdemons <8983377+knightofdemons@users.noreply.github.com>
2021-12-05 19:35:07 +0100

    oauth

--- js/oauth.js
+++ js/oauth.js
@@ -1,19 +1,19 @@
 var popOauth;
 
 function openOauthPopup() {
-	localStorage.setItem('oauth', false);
+	localStorage.setItem('oauthWatcher', false);
 	rqURL = "https://www.bungie.net/en/OAuth/Authorize?client_id=38180&response_type=code";
 	popOauth = window.open(rqURL, "winOauth", "popup, left=200px,top=200px,width=580px,height=700px");
 	popOauth.addEventListener('storage', function(e) {
-		if(localStorage.getItem('oauth') && localStorage.getItem('oauth')) {
+		if(localStorage.getItem('oauthWatcher') && localStorage.getItem('oauthCode')) {
 			// Reload authorization code from LocalStorage
 			console.log("call");
-			localStorage.removeItem('oauth');
+			localStorage.removeItem('oauthWatcher');
 		}
 	});
 
 	// Authorize/Redirect Window
-	if (localStorage.getItem('oauth')) {
+	if (localStorage.getItem('oauthWatcher')) {
 			console.log(popOauth);
 		// Save authorization code to LocalStorage and close the tab
 		// popOauth.close();
